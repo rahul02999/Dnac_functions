a7db5052da5e60cad2fcf9dda08a30b89d4e5160
rahul02999 <47189199+rahul02999@users.noreply.github.com>
2020-02-13 20:43:01 +0530

    Update dnac_functions.py

--- dnac_functions.py
+++ dnac_functions.py
@@ -7,11 +7,7 @@ from prettytable import PrettyTable
 import os,sys
 import re
 import time
-import urllib3
 import json
-
-
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 
@@ -52,25 +48,6 @@ def get_site_health(Burl):
 
 
 
-def get_site_health_all(Burl):
-    """
-    Returns Overall Health information for all sites
-    """
-    url = Burl.format("dna/intent/api/v1/site-health?timestamp=") + str(time.time()*1000)
-    hdr = {'x-auth-token': token, 'content-type': 'application/json'}
-    resp = requests.get(url, headers=hdr,verify=False)  # Make the Get Request
-    site_health = resp.json()
-    dnac_devices = PrettyTable(["siteName", "siteId", "parentSiteName", "siteType", "latitude", "longitude",
-         "healthy Network Device Percentage", "healthy Clients Percentage","number Of Network Device"])
-    dnac_devices.padding_width = 1  # to put width after each entry
-    for item in site_health['response']:
-        dnac_devices.add_row([item["siteName"], item["siteId"], item["parentSiteName"], item["siteType"],
-                              item["latitude"], item["longitude"], item["healthyNetworkDevicePercentage"], item["healthyClientsPercentage"],
-                              item["numberOfNetworkDevice"]])
-    print('\n\n\n\nSite Health:-\n', dnac_devices)
-
-
-
 
 def get_network_device(Burl):
     '''Returns list of network devices '''
@@ -84,37 +61,6 @@ def get_network_device(Burl):
         dnac_devices.add_row([item["hostname"], item["managementIpAddress"], item["serialNumber"], item["platformId"],item["softwareType"], item["softwareVersion"], item["role"], item["upTime"],item["id"]])
     print('\nList of Devices in DNA-\n', dnac_devices)
 
-def get_specific_network_device(host_name,ip_subnet,Burl):
-    url = Burl.format("dna/intent/api/v1/network-device?hostname=") + host_name + ".*&managementIpAddress=" + ip_subnet + ".*"
-    hdr = {'x-auth-token': token, 'content-type': 'application/json'}
-    resp = requests.get(url, headers=hdr,verify=False)  # Make the Get Request
-    network_device = resp.json()
-    dnac_devices = PrettyTable(["Hostname", "Management Ip Address", "Serial Number", "platform Id", "Software Type", "Software Version", "Role", "Up Time"])
-    dnac_devices.padding_width = 1 # to put width after each entry
-    for item in network_device['response']:
-        dnac_devices.add_row([item["hostname"],item["managementIpAddress"],item["serialNumber"],item["platformId"],item["softwareType"],item["softwareVersion"],item["role"],item["upTime"]])
-    print('\n\n\n\n',dnac_devices)
-
-
-def get_device_config(Burl):
-    url = Burl.format("dna/intent/api/v1/network-device/config")
-    hdr = {'x-auth-token': token, 'content-type': 'application/json'}
-    resp = requests.get(url, headers=hdr,verify=False)  # Make the Get Request
-    device_config= resp.json()
-    print_device_config(device_config)
-
-
-def print_device_config(device_config):
-    path = "device-config"
-    if not os.path.exists(path):
-        os.makedirs(path)
-    for i in device_config['response']:
-        filename = re.findall(r"hostname (.*)",i['runningConfig'])
-        print(filename[0])
-        with open(os.path.join(path, filename[0]), 'wt') as temp_file:
-            temp_file.write(i['runningConfig'])
-        print("device id : ", i['id'])
-        print("\n\nRunning-config for device id-'{}' :{}\n\n".format(i['id'],i['runningConfig']))
 
 def get_device_count(Burl):
     url = Burl.format("dna/intent/api/v1/network-device/count")
@@ -123,34 +69,6 @@ def get_device_count(Burl):
     device_count = resp.json()
     print("total number of devices in the setup: ",device_count['response'])
 
-def get_device_by_ip_address(ip_add,Burl):
-    url = Burl.format("dna/intent/api/v1/network-device/ip-address/")+ip_add
-    hdr = {'x-auth-token': token, 'content-type': 'application/json'}
-    resp = requests.get(url, headers=hdr,verify=False)  # Make the Get Request
-    interface_ip = resp.json()
-    dnac_devices = PrettyTable(["Hostname", "Family", "Serial Number", "platform Id", "Software Version", "Role", "Up Time"])
-    dnac_devices.padding_width = 1  # to put width after each entry
-    dnac_devices.add_row([interface_ip['response']["hostname"], interface_ip['response']["family"], interface_ip['response']["serialNumber"], interface_ip['response']["platformId"], interface_ip['response']["softwareVersion"], interface_ip['response']["role"], interface_ip['response']["upTime"]])
-    print('\n\nDevice detail based on ip adderess-',ip_add,'\n', dnac_devices,'\n')
-
-
-def get_device_int(device_id,Burl):
-    url = Burl.format("api/v1/interface")
-    hdr = {'x-auth-token': token, 'content-type': 'application/json'}
-    querystring = {"macAddress": device_id}
-    resp = requests.get(url, headers=hdr, params=querystring,verify=False)  # Make the Get      Request
-    interface_info_json = resp.json()
-    dnac_devices = PrettyTable(["portName", "vlanId",  "portMode", "portType", "duplex", "status","interfaceType", "lastUpdated"])
-    dnac_devices.padding_width = 1  # to put width after each entry
-    for item in interface_info_json['response']:
-        dnac_devices.add_row([item["portName"], item["vlanId"], item["portMode"], item["portType"], item["duplex"], item["status"],item["interfaceType"],item["lastUpdated"]])
-    print(dnac_devices)
-
-def get_device_id(device_json,Burl):
-    for device in device_json['response']:  # Loop through Device List and Retreive DeviceId
-        print("Fetching Interfaces for Device Id ----> {}".format(device['hostname']))
-        get_device_int(device['id'],Burl)
-        print('\n')
 
 def get_device_list(Burl):
     url = Burl.format("api/v1/network-device")
@@ -162,7 +80,6 @@ def get_device_list(Burl):
     for item in device_list['response']:
         dnac_devices.add_row([item["hostname"], item["managementIpAddress"], item["serialNumber"], item["platformId"],item["softwareType"], item["softwareVersion"], item["role"], item["upTime"]])
     print('\n\nPrinting the device list-\n', dnac_devices,'\n\n')
-    get_device_id(device_list,Burl)
 
 def create_area_request(name,parentname,Burl):
     payload={"type": "area",
@@ -200,23 +117,6 @@ def create_building_request(area_name,area_parent,bld_name,area_add,Burl):
         sys.exit(1)
     return response.json()
 
-def get_specific_site_health(site_name,Burl):
-    """
-    Returns Overall Health information for all sites
-    """
-    url = Burl.format("dna/intent/api/v1/site-health?timestamp=") + str(time.time()*1000)
-    hdr = {'x-auth-token': token, 'content-type': 'application/json'}
-    resp = requests.get(url, headers=hdr,verify=False)  # Make the Get Request
-    site_health = resp.json()
-    dnac_devices = PrettyTable(["siteName", "siteId", "parentSiteName", "siteType", "latitude", "longitude",
-         "healthy Network Device Percentage", "healthy Clients Percentage","number Of Network Device"])
-    dnac_devices.padding_width = 1  # to put width after each entry
-    for item in site_health['response']:
-        if item["siteName"] == site_name :
-            dnac_devices.add_row([item["siteName"], item["siteId"], item["parentSiteName"], item["siteType"],
-                              item["latitude"], item["longitude"], item["healthyNetworkDevicePercentage"], item["healthyClientsPercentage"],
-                              item["numberOfNetworkDevice"]])
-    print('\nCreated site detail-\n', dnac_devices,'\n')
 
 def create_site(filename,Burl):
     print("\n\nCreating Area in DNA:")
@@ -366,23 +266,6 @@ def device_delete(device_ip,Burl):
     print("\nDevice deleted successfully-", response.json()['response'])
     print('\n After deleting device- ')
     get_network_device(Burl)
-
-
-
-
-def get_site_topology(Burl):
-    """Returns site topology"""
-    url = Burl.format("dna/intent/api/v1/topology/site-topology")
-    hdr = {'x-auth-token': token, 'content-type': 'application/json'}
-    resp = requests.get(url, headers=hdr, verify=False)  # Make the Get Request
-    site_topo = resp.json()
-    site_topo_table = PrettyTable(["display Name", "group Name Hierarchy", "id", "location Type", "name", "parent Id"])
-    site_topo_table.padding_width = 1  # to put width after each entry
-    for i in range(0,len(site_topo['response']['sites'])):
-        item=site_topo['response']['sites'][i]
-        site_topo_table.add_row([item["displayName"], item["groupNameHierarchy"], item["id"], item["locationType"],item["name"], item["parentId"]])
-        i=i+1
-    print('\n\n', site_topo_table, '\n')
 
 
 
